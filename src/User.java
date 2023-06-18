05e5e1aeea4441143f6d11b807100125d239878a
Tbusk <buskt@ferris.edu>
2023-06-18 05:54:35 -0400

    Adding user account creation feature
    User is populated with getters and setters for username and password.
    EWalletApp CreateUser works for user creation.  To add users, the
    username must not be taken and the password must be complex.

--- src/User.java
+++ src/User.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+/**
+ * A class for creating user objects, which contains currency rates, income, expenses, balance, and monthly savings
+ */
 public class User {
 	private ArrayList <Currency>currencyRates;
 	private ArrayList <Wage>Income;  // user income sources that user can record or view or search by type or month 
@@ -11,5 +14,47 @@ public class User {
 	// possible monthly savings, calculated using monthly income (most recent) assuming the data we have is for one year, and monthly and biweekly expenses, here you can assume yearly expenses that are recorded have already been paid. 
 	double monthlysavings;	
 	//should add constructor(s)
-	User(String username,String password){}
+
+	/**
+	 * When creating a User object, the user needs to provide a username and password.
+	 * @param username user's desired username as String
+	 * @param password user's desired password as String
+	 */
+	User(String username, String password){
+		this.username = username;
+		this.pwd = password;
+	}
+
+	/**
+	 * Method responsible for getting user's username
+	 * @return user's username as String
+	 */
+	protected String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * Method responsible for getting user's password
+	 * @return user's password as String
+	 */
+	protected String getPwd() {
+		return this.pwd;
+	}
+
+	/**
+	 * Method responsible for updating the user's username
+	 * @param username user's desired username
+	 */
+	protected void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Method responsible for updating the user's password
+	 * @param password user's desired password
+	 */
+	protected void setPwd(String password) {
+		this.pwd = password;
+	}
+
 }
