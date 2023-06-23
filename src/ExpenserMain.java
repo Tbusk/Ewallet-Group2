0201409dafc50e0ec9a366d2237a97e7473f434e
EvanSchulte <etschulte04@gmail.com>
2023-06-22 21:21:30 -0400

    Added comments to methods

--- src/ExpenserMain.java
+++ src/ExpenserMain.java
@@ -1,11 +1,13 @@
 
 public abstract class ExpenserMain implements Expenser {
 	
+	// Add Expense feature
 	@Override
 	public void addExpense (Expense Ex) {
 		
 	}
 	
+	// Add Monthly Income feature
 	@Override 
 	public void addMonthlyIncome (Wage W) {
 		
