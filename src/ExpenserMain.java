0e306eb717ed41ad2650d0c55188fef50ade01b5
EvanSchulte <etschulte04@gmail.com>
2023-06-22 21:17:41 -0400

    Made new class and added 2 methods to it

---
+++ src/ExpenserMain.java
@@ -0,0 +1,14 @@
+
+public abstract class ExpenserMain implements Expenser {
+	
+	@Override
+	public void addExpense (Expense Ex) {
+		
+	}
+	
+	@Override 
+	public void addMonthlyIncome (Wage W) {
+		
+	}
+
+}
