bbaaa51423ba223539cf8680ba741cbed817f62a
Trevor Busk <buskt@ferris.edu>
2023-06-26 02:21:12 -0400

    Reapplying User methods to ExpenserMain
    addExpense, addMonthlyIncome, and updateMonthlySavings methods update

--- src/ExpenserMain.java
+++ src/ExpenserMain.java
@@ -7,79 +7,68 @@ public class ExpenserMain implements Expenser {
 	// Add Expense feature
 	@Override
 	public void addExpense (Expense Ex) {
-		
+		userAtHand.addExpense(Ex);
 	}
 	
 	// Add Monthly Income feature
 	@Override 
 	public void addMonthlyIncome (Wage W) {
-		
+		userAtHand.addMonthlyIncome(W);
 	}
 
 	@Override
 	public void PrintFullreport() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void PrintExpensereport() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void PrintIncomereport() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void PrintIncomereportbyTpe() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void PrintExpensebyType() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void exportReport(String reportTitle) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Currency convertForeignCurrency(Currency C, double amount) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean loadExpenseFile(String filePath) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean loadIncomeFile(String filePath) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int whenCanIBuy(String itemname, double price) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void updateMonthlySavings() {
-		// TODO Auto-generated method stub
-		
+		userAtHand.setMonthlySavings(userAtHand.getBalance() - userAtHand.getExpenses());
 	}
 
 }
