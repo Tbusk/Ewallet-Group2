07024f5b76c8c68b8b91dd08d9b249c6481752d5
EvanSchulte <etschulte04@gmail.com>
2023-07-06 17:27:18 -0400

    Fixed layout and added button

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -139,7 +139,7 @@ class appFrame extends JFrame {
 	ExpenserMain expenserMain;
 	JMenuBar navMenuBar;
 	JMenu navMenu;
-	JMenuItem homeNav, addItemNav, importNav, estimateNav, incomeReportNav, expenseReportNav, detailedReportNav, loginNav; // different pages
+	JMenuItem homeNav, addItemNav, importNav, estimateNav, incomeReportNav, expenseReportNav, detailedReportNav, loginNav, createAccNav; // different pages
 
 	appFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -322,6 +322,27 @@ class appFrame extends JFrame {
 				repaint();
 			}
 		});
+		
+		createAccNav = new JMenuItem("<html><p style='margin-left:15'>Create Account"); // Add Items Page
+		createAccNav.addMouseListener(new MouseAdapter() {
+			// once the page is clicked and released, it will be displayed while discarding previous JPanel instead of storing it
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+				super.mouseClicked(e);
+				getContentPane().removeAll();
+				getContentPane().add(lPanel);
+				revalidate();
+				repaint();
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				super.mouseReleased(e);
+				revalidate();
+				repaint();
+			}
+		});
 
 		// Updating font size of menu and menu items
 		navMenu.setFont(new Font(null, Font.PLAIN, 24));
@@ -333,6 +354,7 @@ class appFrame extends JFrame {
 		expenseReportNav.setFont(new Font(null, Font.PLAIN, 20));
 		detailedReportNav.setFont(new Font(null, Font.PLAIN, 20));
 		loginNav.setFont(new Font(null, Font.PLAIN, 20));
+		createAccNav.setFont(new Font(null, Font.PLAIN, 20));
 
 		// Adding items to the navigation menu
 		navMenu.add(homeNav);
@@ -343,6 +365,7 @@ class appFrame extends JFrame {
 		navMenu.add(expenseReportNav);
 		navMenu.add(detailedReportNav);
 		navMenu.add(loginNav);
+		navMenu.add(createAccNav);
 		navMenuBar.add(navMenu);
 		
 
@@ -1358,12 +1381,13 @@ class loginPanel extends JPanel {
 	JLabel usernameLbl, passwordLbl, loginLbl;
 	GridBagConstraints gbConst;
 	JTextField usernameIncField, passwordIncField;
-	JPanel usernamePane, passwordPane;
+	JButton loginBtn;
 	
 	loginPanel() {
 		loginLbl = new JLabel("LOGIN");
 		usernameLbl = new JLabel("Username: ");
 		passwordLbl = new JLabel("Password: ");
+		loginBtn = new JButton("Login");
 		gbConst = new GridBagConstraints();
 		this.setLayout(new GridBagLayout());
 		
@@ -1375,36 +1399,44 @@ class loginPanel extends JPanel {
 		gbConst.gridx = 0;
 		gbConst.gridy = 0;
 		gbConst.gridwidth = 2;
-		gbConst.insets = new Insets(-150,20,60,20);
+		gbConst.insets = new Insets(20,10,20,40);
 		loginLbl.setFont(new Font(null, Font.PLAIN, 44));
 		this.add(loginLbl, gbConst);
 		
 		gbConst.gridx = 1;
-		gbConst.gridy = 1;
-		gbConst.insets = new Insets(-20,30,30,30);
+		gbConst.gridy = 2;
+		gbConst.gridwidth = 1;
+		gbConst.insets = new Insets(0,5,20,30);
 		passwordIncField.setFont(new Font(null, Font.PLAIN, 28));
 		this.add(passwordIncField, gbConst);
-		
+
 		gbConst.gridx = 1;
 		gbConst.gridy = 1;
-		gbConst.insets = new Insets(-124,30,30,30);
+		gbConst.insets = new Insets(0,5,20,30);
 		usernameIncField.setFont(new Font(null, Font.PLAIN, 28));
 		this.add(usernameIncField, gbConst);
-		
+
 		gbConst.gridx = 0;
-		gbConst.gridy = 1;
-		gbConst.gridwidth = 2;
-		gbConst.insets = new Insets(-5,-300,60,20);
+		gbConst.gridy = 2;
+		gbConst.insets = new Insets(0,0,20,20);
 		passwordLbl.setFont(new Font(null, Font.PLAIN, 44));
 		this.add(passwordLbl, gbConst);
-		
+
 		gbConst.gridx = 0;
 		gbConst.gridy = 1;
-		gbConst.gridwidth = 2;
-		gbConst.insets = new Insets(-100,-300,60,20);
+		gbConst.insets = new Insets(0,0,20,20);
 		usernameLbl.setFont(new Font(null, Font.PLAIN, 44));
 		this.add(usernameLbl, gbConst);
+		
+		gbConst.gridx = 0;
+		gbConst.gridy = 4;
+		gbConst.gridwidth = 2;
+		gbConst.insets = new Insets(20,30,30,30);
+		loginBtn.setFont(new Font(null, Font.PLAIN, 28));
+		loginBtn.setPreferredSize(new Dimension(150,60));
+		this.add(loginBtn, gbConst);
 		
 		
 	}
 }
+
